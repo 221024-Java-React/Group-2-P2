5a7bec518e3ebcfb05a75e37f628f409254ae5f2
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-12-01 14:07:38 -0800

    Update StatusBar.tsx

--- social-media-application/src/Components/UserProfile/StatusBar/StatusBar.tsx
+++ social-media-application/src/Components/UserProfile/StatusBar/StatusBar.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './StatusBar.css';
+import axios from 'axios';
+import { userid } from '../../../Util/Users';
+
+const STATUS_POST_URL = "http://localhost:3000/post/create";
 
 const StatusBar = () => {
 
-  const submitHandler = () => {
-    console.log("Submiting Post");
+  const submitHandler = (event : any) => {
+    event.preventDefault();
+
+    console.log(event);
+    console.log(event.target[0].value);
+
+    axios.post(STATUS_POST_URL, {
+      userid: userid,
+      message: event.target[0].value
+    });
   };
 
   return (
     <div className='status-bar'>
-      <form>
+      <form onSubmit={submitHandler}>
         <textarea
           name='status'
           placeholder="What's on your mind?"
           rows={4}
           cols={50}
         ></textarea>
-        <button  onClick={submitHandler} type='submit'>Update</button>
+        <button type='submit'>Update</button>
       </form>
     </div>
   );
