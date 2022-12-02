70d1c89b3856efe2b894c1af1096fe0d830a8b6f
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-12-02 14:01:49 -0800

    Added Register

--- social-media-application/src/Components/Login/Login.tsx
+++ social-media-application/src/Components/Login/Login.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import './Login.css';
 import Navigation from '../Navigation/Navigation';
+import axios from 'axios';
 
 const Login = () => {
+
+  const loginHandler = (event : any) => {
+
+    event.preventDefault();
+
+    axios.post("http://localhost:8090/login", {
+      email: event.target[0].value,
+      password: event.target[1].value
+    }).then((response) => {
+
+      // TODO redirect
+      console.log(response);
+
+    });
+  }
+
   return (
     <>
       <Navigation />
-      <form className='login-form'>
+      <form className='login-form' onSubmit={loginHandler}>
         <h1>Login</h1>
         <input type='text' name='email' placeholder='Email' />
         <input type='password' name='password' placeholder='Password' />
         <button type='submit'>Login</button>
-        <h4>Not a member?</h4>
-        <a href='#'>Sign Up</a>
+        <h4>Not A Member?</h4>
+        <a href='/register'>Sign Up</a>
       </form>
     </>
   );
--- social-media-application/src/Components/UserProfile/UserProfile.tsx
+++ social-media-application/src/Components/UserProfile/UserProfile.tsx
@@ -6,23 +6,51 @@ import StatusBar from './StatusBar/StatusBar';
 import './UserProfile.css';
 import PostContainer from './Posts/PostContainer/PostContainer';
 import Post from './Posts/Post/Post';
-import { users, userid } from '../../Util/Users';
-import { PostData, posts } from '../../Util/Posts';
+import { PostData } from '../../Util/Posts';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { User } from '../../Util/Users';
 
 const UserProfile = () => {
 
+  // const userPosts = posts.map((postData : PostData) => {
+  //   return (postData.userid == userid) ? <Post key={postData.id} post={postData} /> : null;
+  // });
+
+  const [user, setUser] = useState();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    axios.get("http://localhost:8090/user/info").then((response) => {
+
+      // setPosts(response.data);
+
+      // console.log(response.data);
+
+    });
+
+    axios.get("http://localhost:8090/posts/all").then((response) => {
+
+      setPosts(response.data);
+
+      console.log(response.data);
+
+    });
+  }, []);
+
   const userPosts = posts.map((postData : PostData) => {
-    return (postData.userid == userid) ? <Post key={postData.id} post={postData} /> : null;
+    return <Post key={postData.id} post={postData} />;
   });
 
   return (
     <>
       <Navigation />
-      <ProfileBanner profileName={users[userid].profileName} />
+      <ProfileBanner profileName={""} />
       <StatusBar />
       <AboutMe />
       <PostContainer>
-        {userPosts}
+        {/* {userPosts} */}
       </PostContainer>
     </>
   );
